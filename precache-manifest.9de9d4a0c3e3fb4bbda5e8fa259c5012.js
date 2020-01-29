5e5a184d0de62d947b2dc7b6e16e79cd9cc3247e
Santiago <santiagobalay1997@hotmail.com>
2020-01-29 15:24:19 -0300

    Updates

---
+++ precache-manifest.9de9d4a0c3e3fb4bbda5e8fa259c5012.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e434c60b6d66e3a5b2d03119cab53a6",
+    "url": "/react-shirt-store/index.html"
+  },
+  {
+    "revision": "8b8b23d220d833c8ee97",
+    "url": "/react-shirt-store/static/css/main.e7dc4c3e.chunk.css"
+  },
+  {
+    "revision": "4cdc87723e37bb9aabc1",
+    "url": "/react-shirt-store/static/js/2.5244735b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-shirt-store/static/js/2.5244735b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8b8b23d220d833c8ee97",
+    "url": "/react-shirt-store/static/js/main.d462655a.chunk.js"
+  },
+  {
+    "revision": "d1ab617e6aca65b52649",
+    "url": "/react-shirt-store/static/js/runtime-main.2c21a124.js"
+  }
+]);
